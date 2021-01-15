eef4902a64b7e4ea51a6986a089bab4f06ebaae7
Julio <jn5.guape@gmail.com>
2021-01-14 22:48:49 -0300

    update

--- RestClient.cpp
+++ RestClient.cpp
@@ -1,17 +1,59 @@
-#include <RestClient.h>
+#include "RestClient.h"
 
-RestClient :: RestClient(){
-
+RestClient :: RestClient(String server, int port){
+    serverName = server;
+    serverPort = port;
 }
 
-String RestClient :: get(){
+String RestClient :: get(String path){
+    String getBody;
+    if (espClient.connect(serverName.c_str(), serverPort)) {
+        Serial.println("[ INFO ] Conectado a API");
+        //Faz uma requisição HTTP
+        espClient.println("GET "+serverPath+" HTTP/1.1");
+        espClient.println("Host: "+serverName);
+        espClient.println(Header);
+        espClient.println();
 
+        int timoutTimer = 10000;
+        long startTimer = millis();
+        boolean state = false;
+        String getAll;
+      
+        while ((startTimer + timoutTimer) > millis()) {
+            Serial.print(".");
+            delay(100);      
+            while (espClient.available()) {
+                char c = espClient.read();
+                if (c == '\n') {
+                    if (getAll.length()==0) state=true;
+                    getAll = "";
+                }
+                else if (c != '\r') getAll += String(c);
+                if (state==true) getBody += String(c);
+                
+                startTimer = millis();
+            }
+            if (getBody.length()>0) break;
+        }
+
+        espClient.stop();
+        return getBody;
+    } else {
+        Serial.println("[ INFO ] Erro ao se conectar com a API."); //Caso não seja possível obter uma conexao
+        return "Erro";
+    }
 }
 
-String RestClient :: post(){
+String RestClient :: post(String path){
 
 }
 
 void RestClient:: st_header(String nHeader){
     Header = nHeader;
+}
+
+void RestClient:: set_server(String server, int port){
+    serverName = server;
+    serverPort = port;
 }
--- RestClient.h
+++ RestClient.h
@@ -1,15 +1,16 @@
 #include <Arduino.h>
-#include <Wifi.h>
+#include <WiFi.h>
 
-#ifndef RestClient
-#define RestClient
+#ifndef RESTCLIENT_H
+#define RESTCLIENT_H
 
 class RestClient{
     public:
-        RestClient();
-        set_header();
-        get();
-        post();
+        RestClient(String server, int port);
+        set_header(String nHeader);
+        set_server(String server, int port)
+        get(String path);
+        post(String path);
     private:
         String serverName, serverPath, Header = "";
         int serverPort;
